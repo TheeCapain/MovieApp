8817963d1576f7fc3f9ca9cba1dbf9321dc66e8e
August <Augusthauerslev@outlook.com>
2022-04-05 18:02:39 +0200

    added hover animation and header

--- Backend/src/main/java/com/example/movieappbackend/config/initData.java
+++ Backend/src/main/java/com/example/movieappbackend/config/initData.java
@@ -19,5 +19,25 @@ public class initData implements CommandLineRunner {
         movie.setMovieTitle("Harry Potter");
         movie.setMoviePosterHref("https://static.posters.cz/image/750webp/105075.webp");
         movieService.saveMovie(movie);
+
+        Movie movie1 = new Movie();
+        movie1.setMovieTitle("Indiana Jones and the temple of Doom");
+        movie1.setMoviePosterHref("https://image.posterlounge.dk/img/products/480000/477816/477816_poster_l.jpg");
+        movieService.saveMovie(movie1);
+
+        Movie movie2 = new Movie();
+        movie2.setMovieTitle("Star Wars: The Last Jedi");
+        movie2.setMoviePosterHref("https://static.posters.cz/image/750webp/97646.webp");
+        movieService.saveMovie(movie2);
+
+        Movie movie3 = new Movie();
+        movie3.setMovieTitle("Tron Legacy");
+        movie3.setMoviePosterHref("https://m.media-amazon.com/images/I/91ZueyN4XOL._AC_SY679_.jpg");
+        movieService.saveMovie(movie3);
+
+        Movie movie4 = new Movie();
+        movie4.setMovieTitle("Kung fu panda 3");
+        movie4.setMoviePosterHref("https://www.scope.dk/shared/11/954/china-film-kung-fu-panda-3-org.vers.-3-d_400x600c.jpg");
+        movieService.saveMovie(movie4);
     }
 }
